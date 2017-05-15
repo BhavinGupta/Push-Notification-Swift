04454e2c87b002b3eab838093aa0df2c27a8e2ea
Bhavin Gupta <bhavingupta007@yahoo.co.in>
2017-05-15 14:21:56 +0530

    Push Notification Swift
    Push notification using swift for iOS 8, 9 and 10 with demo entitlement
    for the created certificates and provisioning profile as per the
    documented sites.

---
+++ DemoPushNotification/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DemoPushNotification
+//
+//  Created by Bhavin Gupta on 30/01/17.
+//  Copyright © 2017 Easy Pay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK:- View Life Cycle 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+}
+
